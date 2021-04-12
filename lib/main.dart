8a82e3f680acfdec01b7bd48b22166ee6d7765f9
unknown <addi_mohammed_said@outlook.fr>
2021-04-12 23:25:22 +0200

    added translations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
@@ -8,17 +9,33 @@ import 'package:get/get.dart';
 import 'app/app.binding.dart';
 import 'app/app.page.dart';
 import 'app/core/theme/app_theme.dart';
+import 'app/core/translations/app_translation.dart';
 
 void main() async {
+  // firebase initialize
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // Run the app
   runZonedGuarded(() {
-    runApp(GetMaterialApp(
-      debugShowCheckedModeBanner: false,
+    runApp(MyApp());
+  }, FirebaseCrashlytics.instance.recordError);
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       initialBinding: AppBinding(),
+      title: 'Flutter Get Fire',
+      debugShowCheckedModeBanner: false,
       defaultTransition: Transition.fade,
       theme: appThemeData,
+      locale: window.locale,
+      fallbackLocale: AppTranslation.fallbackLocale,
+      translations: AppTranslation(),
       home: AppPage(),
-    ));
-  }, FirebaseCrashlytics.instance.recordError);
+    );
+  }
 }
