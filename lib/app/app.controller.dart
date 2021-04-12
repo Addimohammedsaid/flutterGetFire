0a4bde26bdef6047fcbd57e19c91f3d553225291
unknown <addi_mohammed_said@outlook.fr>
2021-04-12 22:02:38 +0200

    chore : added firebase analytics

--- lib/app/app.controller.dart
+++ lib/app/app.controller.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:get/get.dart';
 
 class AppController extends GetxController {
-  final _obj = ''.obs;
-  set obj(value) => this._obj.value = value;
-  get obj => this._obj.value;
+  // var for automatically start collecting data
+  FirebaseAnalytics analytics;
+
+  @override
+  onInit() {
+    // start collecting one intilized
+    analytics = new FirebaseAnalytics();
+    super.onInit();
+  }
 }
